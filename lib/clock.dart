a09f00ac9917614e9888c8fb7eb3d294d15dce52
VengAnn <venganncoco@gmail.com>
2023-07-06 23:27:21 +0700

    update

--- lib/clock.dart
+++ lib/clock.dart
@@ -1,4 +1,8 @@
+import 'dart:async';
+import 'dart:math';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:intl/intl.dart'; // Add this import statement
 
 class Clock extends StatefulWidget {
   const Clock({Key? key}) : super(key: key);
@@ -8,86 +12,147 @@ class Clock extends StatefulWidget {
 }
 
 class _ClockState extends State<Clock> {
-  double minutesAngle = 0;
-  double secondsAngle = 0;
-  double hoursAngle = 0;
+  Timer? timer;
+  late DateTime currentTime;
+
+  @override
+  void initState() {
+    super.initState();
+    currentTime = DateTime.now();
+    timer = Timer.periodic(Duration(seconds: 1), (timer) {
+      setState(() {
+        currentTime = DateTime.now();
+      });
+    });
+  }
+
+  @override
+  void dispose() {
+    timer?.cancel();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: const Color.fromRGBO(8, 25, 35, 1),
-      alignment: const Alignment(0, 0),
-      child: Container(
-        width: 370,
-        height: 370,
-        decoration: BoxDecoration(
-            border: Border.all(color: Colors.black45, width: 9),
-            borderRadius: BorderRadius.circular(200)),
-        child: Stack(
-          children: <Widget>[
-            Image.network(
-              'https://png.pngtree.com/png-clipart/20220705/ourmid/pngtree-clock-face-no-hand-roboto-font-png-image_5683165.png',
-            ),
-            //Hour
-            Transform.rotate(
-              angle: hoursAngle,
-              child: Container(
-                alignment: Alignment(0, -0.40),
-                child: Container(
-                  height: 80,
-                  width: 10,
-                  decoration: BoxDecoration(
-                    color: Colors.pink,
-                    borderRadius: BorderRadius.circular(10),
-                  ),
-                ),
-              ),
-            ),
-
-            //Minutes
-            Transform.rotate(
-              angle: minutesAngle,
-              child: Container(
-                alignment: Alignment(0, -0.40),
-                child: Container(
-                  height: 100,
-                  width: 5,
-                  decoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.circular(10),
-                  ),
-                ),
-              ),
-            ),
-            // Seconds
-            Transform.rotate(
-              angle: secondsAngle,
-              child: Container(
-                alignment: Alignment(0, -0.40),
-                child: Container(
-                  height: 150,
-                  width: 2,
-                  decoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.circular(10),
-                  ),
-                ),
-              ),
-            ),
-            // Dot
-            Container(
-              alignment: Alignment(0, 0),
-              child: Container(
-                height: 15,
-                width: 15,
-                decoration: BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.circular(100),
-                ),
-              ),
-            ),
-          ],
+    final double secondsAngle = (2 * pi / 60) * currentTime.second;
+    final double minutesAngle = (2 * pi / 60) * currentTime.minute;
+    final double hoursAngle = (2 * pi / 12) * (currentTime.hour % 12) +
+        (2 * pi / (12 * 60)) * currentTime.minute;
+    final currentDate = DateFormat('EEE, MMM d, yyyy')
+        .format(currentTime); // Format the current date
+    final currentTimeString = DateFormat('HH:mm:ss').format(DateTime.now());
+    // Set the status bar color and brightness
+    SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
+      statusBarColor: Colors.blue, // Set the status bar color
+      statusBarBrightness: Brightness.dark, // Set the status bar icon color
+    ));
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          currentDate,
+          style: const TextStyle(
+            fontSize: 24, // Set the font size
+            color: Colors.blue,
+          ),
         ),
+        backgroundColor: const Color.fromARGB(255, 173, 238, 32),
       ),
+      body: Column(
+          //
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+                alignment: Alignment.center,
+                child: Container(
+                  width: 360,
+                  height: 360,
+                  decoration: BoxDecoration(
+                    color: Color.fromARGB(255, 126, 177, 60),
+                    border: Border.all(color: Colors.black45, width: 9),
+                    borderRadius: BorderRadius.circular(180),
+                  ),
+                  child: Stack(
+                    children: <Widget>[
+                      Image.network(
+                        'https://png.pngtree.com/png-clipart/20220705/ourmid/pngtree-clock-face-no-hand-roboto-font-png-image_5683165.png',
+                      ),
+                      // Hour
+                      Transform.rotate(
+                        angle: hoursAngle,
+                        alignment: Alignment.center,
+                        child: Container(
+                          alignment: Alignment(0, -0.35),
+                          child: Container(
+                            height: 80,
+                            width: 8,
+                            decoration: BoxDecoration(
+                              color: Colors.pink,
+                              borderRadius: BorderRadius.circular(10),
+                            ),
+                          ),
+                        ),
+                      ),
+                      // Minutes
+                      Transform.rotate(
+                        angle: minutesAngle,
+                        alignment: Alignment.center,
+                        child: Container(
+                          alignment: Alignment(0, -0.45),
+                          child: Container(
+                            height: 110,
+                            width: 5,
+                            decoration: BoxDecoration(
+                              color: Colors.blue,
+                              borderRadius: BorderRadius.circular(10),
+                            ),
+                          ),
+                        ),
+                      ),
+                      // Seconds
+                      Transform.rotate(
+                        angle: secondsAngle,
+                        alignment: Alignment.center,
+                        child: Container(
+                          alignment: Alignment(0, -0.40),
+                          child: Container(
+                            height: 140,
+                            width: 2,
+                            decoration: BoxDecoration(
+                              color: Colors.white,
+                              borderRadius: BorderRadius.circular(10),
+                            ),
+                          ),
+                        ),
+                      ),
+                      // Dot
+                      Container(
+                        alignment: Alignment.center,
+                        child: Container(
+                          height: 15,
+                          width: 15,
+                          decoration: BoxDecoration(
+                            border: Border.all(color: Colors.white, width: 2),
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                        ),
+                      ),
+                      Positioned(
+                        left: 120,
+                        bottom: 100,
+                        //right: 50,
+                        child: Text(
+                          "${currentTimeString}",
+                          style: const TextStyle(
+                            fontSize: 24,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.black,
+                          ),
+                        ),
+                      )
+                    ],
+                  ),
+                )),
+          ]),
     );
   }
 }
